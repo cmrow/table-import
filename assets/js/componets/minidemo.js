ce80823946d2b681b706e6b668cc237f18e990d0
Carlos Rodriguez <cmrodriguez261@misena.edu.co>
2020-06-02 20:41:56 -0500

    tabla reutilizable ok

--- assets/js/componets/minidemo.js
+++ assets/js/componets/minidemo.js
@@ -16,135 +16,134 @@ class ComponenteImportaciones extends HTMLElement {
     connectedCallback() {
         this._root.innerHTML =
             `<style>
-            table {
-                border-collapse: collapse;
-                margin: 0px;
-                border-bottom: 0px;
-            }
-            th {
-                height: 40px;
-                border-right: var(--white) solid 1px;
-                padding: 1em;
-            }
-            th:last-child, td:last-child {
-                border-right: 0px;
-            }
-            tr:last-child {
-                border-bottom: 0px;
-                border-right: 0px;
-            }
-            td:first-child {
-                border-left: 0px;
-            }
-            td {
-                border: 1px solid var(--grey_line);
-                padding: 2px;
-                text-align: center;
-            }
-            tr{
-                min-height: 40px;
-            }
-            .selected {
-                background-color: var(--green_soft);
-            }
-            tbody tr:hover {
-                background-color: var(--green_soft);
-            }
-            tbody {
-                background-color: var(--grey_bg);
-            }
-            .contenedor-componente {
-                font-family: Verdana, Geneva, Tahoma, sans-serif;
-                display: flex;
-                flex-direction: column;
-                background-color: var(--white);
-                font-size: small;
-                margin: 1em;
-                border: solid var(--grey_thead) 1px;
-            }
-            .botons {
-                display: flex;
-            }
-            
-            .btn-crear:hover {
-                border: 1px;
-                background: var(--grey_thead);
-                border-style: inset;
-            }
-            .btn-accion {
-                display: flex;
-                justify-content: center;
-                align-items: center;
-                height: 40px;
-                width: 40px;
-                background: var(--bg_soft);
-                border-radius: 1px;
-                cursor: pointer;
-                border: solid var(--grey_bg) 1px;
-            }
-            .btn-accion:hover {
-                background-color: #E2E7FC;
-                border: var(--grey_bg) 1px;
-                border-style: outset;
-            }
-            .main-thead {
-                background-color: var(--grey_thead);
-                color: var(--white);
-                width: 100%;
-            }
-            .header-componente {
-                background: #104F863B;
-                display: flex;
-                justify-content: space-between;
-                align-items: center;
-                padding: 2em;
-                color: var(--grey_thead);
-                /* border-bottom: solid 2px; */
-                /* border-color: var(--grey_line); */
-            }
-            .title {
-                font-size: 2rem;
-            }
-            div.btn-crear {
-                background-color: var(--blue_sinco);
-                display: flex;
-                flex-wrap: wrap;
-                justify-content: flex-start;
-                align-items: center;
-                margin-left: 1em;
-                padding: 0 1em 0 1em;
-                color: #FFFFFF;
-                border-radius: 1px;
-                height: 40px;
-                width: 160px;
-                border: solid var(--grey_bg) 1px;
-                cursor: pointer;
-            }
-        </style>
-        <div class="contenedor-componente">
-            <header class="header-componente">
-                <h1 class="title"></h1>
-                <div class="botons">
-                    <div class="btn-accion" title="Auditoria">
-                        <img src="../assets/css/icons/auditoria.svg" alt="">
-                    </div>
-                    <div class="btn-accion" title="Editar">
-                        <img src="../assets/css/icons/edit.svg" alt="">
-                    </div>
-                    <div class="btn-accion" title="Borrar">
-                        <img src="../assets/css/icons/borrar.svg" alt="">
-                    </div>
-                    <div class="btn-crear" title="Crear Importaciones">
-                        <img src="../assets/css/icons/plus.svg" alt="">
-                        Crear importación
-                    </div>
+        table {
+            border-collapse: collapse;
+            margin: 0px;
+            border-bottom: 0px;
+            width: 100%;
+        }
+        th {
+            height: 1em;
+            border-right: var(--white) solid 1px;
+            padding: 1em;
+        }
+        th:last-child, td:last-child {
+            border-right: 0px;
+        }
+        tr:last-child {
+            border-bottom: 0px;
+            border-right: 0px;
+        }
+        td:first-child {
+            border-left: 0px;
+        }
+        tr > td {
+            border: 1px solid var(--grey_line);
+            padding: 2px;
+            text-align: center;
+            min-height: 2em;
+        }
+        .selected {
+            background-color: var(--green_soft);
+        }
+        tbody tr:hover {
+            background-color: var(--green_soft);
+        }
+        tbody {
+            background-color: var(--grey_bg);
+        }
+        .contenedor-componente {
+            font-family: Verdana, Geneva, Tahoma, sans-serif;
+            display: flex;
+            flex-direction: column;
+            background-color: var(--white);
+            font-size: smaller;
+            margin: 1em;
+            border: solid var(--grey_thead) 1px;
+        }
+        .botons {
+            display: flex;
+        }
+        
+        .btn-crear:hover {
+            border: 1px;
+            background: var(--grey_thead);
+            border-style: inset;
+        }
+        .btn-accion {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 3em;
+            width: 3em;
+            background: var(--bg_soft);
+            border-radius: 1px;
+            cursor: pointer;
+            border: solid var(--grey_bg) 1px;
+        }
+        .btn-accion:hover {
+            background-color: #E2E7FC;
+            border: var(--grey_bg) 1px;
+            border-style: outset;
+        }
+        .main-thead {
+            background-color: var(--grey_thead);
+            color: var(--white);
+            width: 100%;
+        }
+        .header-componente {
+            background: #104F863B;
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+            padding: 1em;
+            color: var(--grey_thead);
+            /* border-bottom: solid 2px; */
+            /* border-color: var(--grey_line); */
+        }
+        .title {
+            font-size: 2rem;
+        }
+        div.btn-crear {
+            background-color: var(--blue_sinco);
+            display: flex;
+            flex-wrap: wrap;
+            justify-content: flex-start;
+            align-items: center;
+            margin-left: 1em;
+            padding: 0 1em 0 1em;
+            color: #FFFFFF;
+            border-radius: 1px;
+            height: 3em;
+            width: 10em;
+            border: solid var(--grey_bg) 1px;
+            cursor: pointer;
+        }
+    </style>
+    <div class="contenedor-componente">
+        <header class="header-componente">
+            <h1 class="title"></h1>
+            <div class="botons">
+                <div class="btn-accion" title="Auditoria">
+                    <img src="../assets/css/icons/auditoria.svg" alt="">
                 </div>
-            </header>
-            <section class="tabla">
+                <div class="btn-accion" title="Editar">
+                    <img src="../assets/css/icons/edit.svg" alt="">
+                </div>
+                <div class="btn-accion" title="Borrar">
+                    <img src="../assets/css/icons/borrar.svg" alt="">
+                </div>
+                <div class="btn-crear" title="Crear Importaciones">
+                    <img src="../assets/css/icons/plus.svg" alt="">
+                    Crear importación
+                </div>
+            </div>
+        </header>
+        <section class="tabla">
 
-            </section>
-        </div>
-        `;
+        </section>
+    </div>
+    `;
         this._$title = this._root.querySelector('.title');
         this._$section_tabla = this._root.querySelector('.tabla');
         this._setTitle(this.getAttribute("titulo"));
